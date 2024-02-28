c31f1876eedf07b7b3cefbb1848fecdaf0faac5a
erosalpe <eros.alpegiani@gmail.com>
2024-02-28 10:29:29 +0100

    commentati var_dump

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php
     session_start();
     function passwordGen($passwordLength){
-        var_dump($_SESSION['lettere'], $_SESSION['numeri'], $_SESSION['simboli']);
+        /*var_dump($_SESSION['lettere'], $_SESSION['numeri'], $_SESSION['simboli']);*/
         $numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
         $lettereMinuscole = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
         $lettereMaiuscole = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
         $specialChar = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
         $passwordGenerata = '';
         for( $i = 0; $i < $passwordLength - 1; $i++ ){
-            var_dump($i, $passwordGenerata);
+            /*var_dump($i, $passwordGenerata);*/
             $scelta = rand(1, 4);
             switch ($scelta){
                 case 1:
